1b13be439a77cbca6297723182b758507c7f13b3
zach <etta1055@gmail.com>
2025-08-11 16:16:31 -0500

    sitemap

---
+++ sitemap-generator.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import { SitemapStream, streamToPromise } from 'sitemap';
+
+const links = [
+    { url: '/', changefreq: 'monthly', priority: 1.0 },
+    { url: '/merch', changefreq: 'weekly', priority: 0.8 },
+    { url: '/contact-us', changefreq: 'monthly', priority: 0.7 },
+    { url: '/our-story', changefreq: 'monthly', priority: 0.7 },
+    { url: '/faq-page', changefreq: 'monthly', priority: 0.7 },
+    { url: '/cart', changefreq: 'weekly', priority: 0.5 },
+    // These routes are for internal Stripe redirects and typically don't need to be in the sitemap
+    // { url: '/success', changefreq: 'never', priority: 0.1 },
+    // { url: '/cancel', changefreq: 'never', priority: 0.1 },
+  ];
+
+const sitemap = new SitemapStream({ hostname: 'https://clawanddecay.com' });
+
+streamToPromise(sitemap).then((data) =>
+  fs.writeFileSync('./public/sitemap.xml', data.toString())
+);
+
+links.forEach(link => sitemap.write(link));
+sitemap.end();
